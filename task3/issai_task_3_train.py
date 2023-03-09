07307af3ce5a39b83754e921edad39d69da1b63b
Assel Yermekova <71093827+Allessyer@users.noreply.github.com>
2023-03-09 16:05:03 +0300

    Add files via upload

--- task3/issai_task_3_train.py
+++ task3/issai_task_3_train.py
@@ -1,8 +1,9 @@
 from speaker_verification import transforms as T
 from speaker_verification.dataset import SpeakingFacesDataset
+from speaker_verification.dataset import ValidDataset
 from speaker_verification.sampler import ProtoSampler
 from speaker_verification.sampler import ValidSampler
-from speaker_verification.models_handmade.resnet import ResNet34
+from speaker_verification.models import ResNet
 from speaker_verification.loss import PrototypicalLoss
 from speaker_verification.train import train_model
 from speaker_verification.parser import createParser
@@ -11,7 +12,11 @@ import torch
 from torch.utils.data import DataLoader
 import numpy as np
 import random
-  
+import timm
+
+from timeit import default_timer as timer
+
+
 if __name__== "__main__":
 
     parser = createParser()
@@ -26,12 +31,20 @@ if __name__== "__main__":
     # dataset
     ANNOTATIONS_FILE = namespace.annotation_file
     DATASET_DIR = namespace.dataset_dir
+    PATH2DATASET = "/workdir/sf_pv"
 
     # train_dataloader
     n_batch = namespace.n_batch
     n_ways = namespace.n_ways
     n_support = namespace.n_support
     n_query = namespace.n_query
+
+    # loss
+    dist_type = namespace.dist_type
+
+    # model
+    model_choice = namespace.model_choice
+    exp_name = namespace.exp_name
 
     # train 
     num_epochs = namespace.num_epochs
@@ -42,14 +55,17 @@ if __name__== "__main__":
     input_parameters["seed_number"] = seed_number
     input_parameters["annotation_file"] = ANNOTATIONS_FILE
     input_parameters["dataset_dir"] = DATASET_DIR
+    input_parameters["path2dataset"] = PATH2DATASET
     input_parameters["n_batch"] = n_batch
     input_parameters["n_ways"] = n_ways
     input_parameters["n_support"] = n_support
     input_parameters["n_query"] = n_query
+    input_parameters["model_choice"] = model_choice
+    input_parameters["exp_name"] = exp_name
     input_parameters["num_epochs"] = num_epochs
     input_parameters["save_dir"] = save_dir
 
-    torch.save(input_parameters,f'{save_dir}/input_parameters')
+    torch.save(input_parameters,f'{save_dir}/{exp_name}_input_parameters')
     #------------------------------------------------------------------
 
     torch.manual_seed(seed_number)
@@ -61,44 +77,62 @@ if __name__== "__main__":
 
     device = torch.device(f"cuda:{str(n_gpu)}" if torch.cuda.is_available() else "cpu")
 
+    # model
+    if model_choice == "resnet1":
+        model = ResNet(pretrained_weights=True, 
+                        fine_tune=False, 
+                        embedding_size=128, 
+                        modality = "rgb", 
+                        filter_size="default", 
+                        from_torch=True
+                    )
+        image_transform = T.image_transform
+        
+    elif model_choice == "resnet2":
+        model = timm.create_model('resnet34', pretrained=True, num_classes=128)
+        image_transform = T.image_transform
+
+    elif model_choice == "vit1":
+        model = timm.create_model("vit_base_patch16_224", pretrained=True, num_classes=128)
+        img_transform  = T.Image_Transforms(model)
+        image_transform = img_transform.timm
+
+    model = model.to(device)
+
     # Dataset
     train_dataset = SpeakingFacesDataset(ANNOTATIONS_FILE,DATASET_DIR,'train',
-                                        image_transform=T.image_transform, 
+                                        image_transform=image_transform, 
                                         audio_transform=T.audio_transform)
-    valid_dataset = SpeakingFacesDataset(ANNOTATIONS_FILE,DATASET_DIR,'valid',
-                                        image_transform=T.image_transform, 
-                                        audio_transform=T.audio_transform)
+    valid_dataset = ValidDataset(PATH2DATASET,'valid',
+                            image_transform=image_transform, 
+                            audio_transform=T.audio_transform)
 
     # sampler
-    train_sampler = ProtoSampler(labels = train_dataset.labels,
-                                        n_batch = 10,
-                                        n_ways = 3, # n_way
-                                        n_support = 1, # n_shots
-                                        n_query = 1)
-                                        
-    val_sampler = ValidSampler(labels = valid_dataset.labels)
+    train_sampler = ProtoSampler(train_dataset.labels,
+                                n_batch,
+                                n_ways, # n_way
+                                n_support, # n_shots
+                                n_query)
 
     # dataloader
     train_dataloader = DataLoader(dataset=train_dataset, 
                             batch_sampler=train_sampler,
-                            num_workers=2, pin_memory=True
+                            num_workers=4, pin_memory=True
                             )
 
-    valid_dataloader = DataLoader(dataset = valid_dataset,
-                                batch_sampler=val_sampler,
-                                num_workers=4, pin_memory=True
-                                )
-    # model
-    model = ResNet34()
-    model = model.to(device)
-
-    # optimizer + scheduler
-    optimizer = torch.optim.Adam(model.parameters(), lr = 0.001, weight_decay = 0)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.5, last_epoch=-1)
+    valid_dataloader = DataLoader(dataset=valid_dataset,
+                            batch_size=64,
+                            shuffle=True,
+                            num_workers=4, 
+                            pin_memory=True)
 
     # loss
-    criterion = PrototypicalLoss(dist_type='cosine_similarity')
+    criterion = PrototypicalLoss(dist_type=dist_type)
     criterion = criterion.to(device)
+
+    # optimizer + scheduler
+    optimizer = torch.optim.Adam(model.parameters(), lr = 1e-3, weight_decay = 0)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.95)
 
     # train
     model = train_model(model,
@@ -110,7 +144,8 @@ if __name__== "__main__":
                         scheduler,
                         device,
                         num_epochs,
-                        save_dir)
+                        save_dir,
+                        exp_name)
 
 
 
